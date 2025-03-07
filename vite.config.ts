35c6bc516b7c24b62dfc91f767796d4b0d7eaab6
Nathan Razafindrakoto <Nathan.Raza18@gmail.com>
2025-03-07 08:34:54 +0100

    gh page

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/Portfolio/', // Remplace "nom-du-repo" par le nom exact de ton repo GitHub
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
